6952ca8381b9d9283f2ef8eda2e798014268c369
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-24 22:02:28 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts "Hey #{name}; what's cracking?"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Dude #{name}--you seriously code in #{language}?"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
